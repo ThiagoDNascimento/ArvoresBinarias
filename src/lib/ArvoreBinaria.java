9a70a31f86d00019590d747ce3edd56039b1f485
Thiago Dutra Nascimento <thiagoduna@hotmail.com>
2024-04-02 08:28:45 -0300

    Update and rename ArvoreBinariaExemplo.java to ArvoreBinaria.java

--- src/lib/ArvoreBinaria.java
+++ src/lib/ArvoreBinaria.java
@@ -12,12 +12,12 @@ import java.util.Comparator;
  *
  * @author victoriocarvalho
  */
-public class ArvoreBinariaExemplo<T> implements IArvoreBinaria<T> {
+public class ArvoreBinaria<T> implements IArvoreBinaria<T> {
     
-    protected NoExemplo<T> raiz = null;
+    protected No<T> raiz = null;
     protected Comparator<T> comparador; 
   
-    public ArvoreBinariaExemplo(Comparator<T> comp) {
+    public ArvoreBinaria(Comparator<T> comp) {
         comparador = comp;
     }
     
